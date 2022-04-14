28a466ebc0f73e56e607d48e71d658df4cb0b8b7
cesaromano <cerodema96@gmail.com>
2022-04-14 13:15:41 -0300

    nimensions.py file added

---
+++ numpy_pandas/dimensions.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+#matrix is composed by columns(atributes) and rows(instances)
+
+#tensor is composed by columns(atributes), rows(time or serie) and examples
+
+#tensor 4D images, columns(height), rows(weight), chanels and examples
+
+scalar = np.array(56)
+
+#print dimension number
+print(f"Dimension number: {scalar.ndim}")
+
+vector = np.array([1, 2, 3, 4, 5])
+
+print(f"Dimension number: {vector.ndim}")
+
+matrix = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+
+print(f"Dimension number: {matrix.ndim}")
+
+tensor = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]], [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]])
+
+print(f"Dimension number: {tensor.ndim}")
+
+#Adding or deleting dimensions when dfining array
+
+vector = np.array([1, 2, 3], ndmin=10)
+print(vector)
+print(f"Dimension number: {vector.ndim}")
